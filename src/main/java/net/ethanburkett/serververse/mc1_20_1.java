dc4084cbf5b63d1b0ced9620ee6b7c407f616575
Ethan Burkett <ethan@ethanburkett.net>
2024-01-07 11:48:46 -0600

    Initial Commit

---
+++ src/main/java/net/ethanburkett/serververse/mc1_20_1.java
@@ -0,0 +1,17 @@
+package net.ethanburkett.serververse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class mc1_20_1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
